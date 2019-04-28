a9ba42dc2e7f8bcc074b50c2a75c6ba328ece7c4
Jeressia Williamson <jeressiajaytv@gmail.com>
2019-04-28 09:08:17 -0500

    dark mode works

--- src/javascripts/components/darkmode/darkmode.js
+++ src/javascripts/components/darkmode/darkmode.js
@@ -1,15 +1,19 @@
 function darkTheme() {
   document.querySelector('.dark-mode, .dark-m-body').style = 'background-color: #303030 !important; color: white; border:none';
   document.querySelector('.dark-nav').style = 'background-color: #505050 !important; color: white; border:none';
+  document.querySelector('.dark-menu').style = 'background-color: #808080 !important; color: white; border:none';
   document.querySelector('.dropdown-item').style = 'background-color: #505050 !important; color: white; border:none';
   document.querySelector('.dark-m-header').style = 'background-color: #505050 !important; color: white; border:none; border-radius: 0px;';
   document.querySelector('.dark-m-body').style = 'background-color: #303030 !important; color: white; border:none';
   document.querySelector('.dark-footer').style = 'background-color: #505050 !important; color: white; border:none;border-radius: 0px';
-  document.querySelector('.btn-dark').style = 'color: white !important;';
+  document.querySelector('.btn-dark').style = 'color: black !important;';
+  document.querySelector('.btn1').style = 'color: black !important;';
+  document.querySelector('.btn2').style = 'color: black !important;';
 }
 
 function lightTheme() {
   document.querySelector('.dark-mode').style = '';
+  document.querySelector('.dark-menu').style = '';
   document.querySelector('.dark-nav').style = '';
   document.querySelector('.dark-m-header').style = '';
   document.querySelector('.dark-m-body').style = '';
