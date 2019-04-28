b38b2eef9e06de25365f9b3473c1eb5228f9ae6a
Jeressia Williamson <jeressiajaytv@gmail.com>
2019-04-28 01:59:56 -0500

    wip

--- src/javascripts/components/darkmode/darkmode.js
+++ src/javascripts/components/darkmode/darkmode.js
@@ -1,4 +1,27 @@
-function darkmode() {
-  document.getElementById('body').className = 'dark-mode';
+function darkTheme() {
+  document.querySelector('.dark-mode, .dark-m-body').style = 'background-color: #303030 !important; color: white; border:none';
+  document.querySelector('.dark-nav').style = 'background-color: #505050 !important; color: white; border:none';
+  document.querySelector('.dark-m-header').style = 'background-color: #505050 !important; color: white; border:none; border-radius: 0px;';
+  document.querySelector('.dark-m-body').style = 'background-color: #303030 !important; color: white; border:none';
+  document.querySelector('.dark-footer').style = 'background-color: #505050 !important; color: white; border:none;border-radius: 0px';
+  document.querySelector('.btn-dark').style = 'color: white !important;';
 }
-export default { darkmode };
+
+function lightTheme() {
+  document.querySelector('.dark-mode').style = '';
+  document.querySelector('.dark-nav').style = '';
+  document.querySelector('.dark-m-header').style = '';
+  document.querySelector('.dark-m-body').style = '';
+  document.querySelector('.dark-footer').style = '';
+  document.querySelector('.btn-dark').style = '';
+}
+
+const themeButtons = () => {
+  const darkButton = document.getElementById('dark-btn');
+  const lightButton = document.getElementById('light-btn');
+
+  darkButton.addEventListener('click', darkTheme);
+  lightButton.addEventListener('click', lightTheme);
+};
+
+export default { themeButtons };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,10 +1,12 @@
 import onLoad from './components/messages';
+import themes from './components/darkmode/darkmode';
 
 import 'bootstrap';
 import '../styles/main.scss';
 
 const init = () => {
   onLoad.getData();
+  themes.themeButtons();
 };
 
 init();
