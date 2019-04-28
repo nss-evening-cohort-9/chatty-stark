f98bec81292a8788b888e20e9386c65c2c4bdf66
Samuel <mrto0ns@live.com>
2019-04-27 21:11:11 -0500

    delete message functionality

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,10 +1,12 @@
 import onLoad from './components/messages';
+import events from './helpers/attach-events';
 
 import 'bootstrap';
 import '../styles/main.scss';
 
 const init = () => {
   onLoad.getData();
+  events.attachEvents();
 };
 
 init();
