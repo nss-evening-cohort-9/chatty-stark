b9660ecb3ddef6635bcfc05c5e85a7c6e55fab9b
Jeressia Williamson <jeressiajaytv@gmail.com>
2019-04-29 19:34:00 -0500

    darkmode simplified

--- src/javascripts/components/darkmode/darkmode.js
+++ src/javascripts/components/darkmode/darkmode.js
@@ -1,37 +1,3 @@
-// function darkTheme() {
-//   document.querySelector('.dark-mode, .dark-m-body').style =
-//  'background-color: #303030 !important; color: white; border:none';
-//   document.querySelector('.dark-nav').style =
-//  'background-color: #505050 !important; color: white; border:none';
-//   document.querySelector('.dark-menu').style =
-//  'background-color: #808080 !important; color: white; border:none';
-//   document.querySelector('.dropdown-item').style =
-//   'background-color: #505050 !important; color: white; border:none';
-//   document.querySelector('.dark-m-header').style =
-//  'background-color: #505050 !important; color: white; border:none; border-radius: 0px;';
-//   document.querySelector('.dark-m-body').style =
-//   'background-color: #303030 !important; color: white; border:none';
-//   document.querySelector('.dark-footer').style =
-//   'background-color: #505050 !important; color: white; border:none;border-radius: 0px';
-//   document.querySelector('.btn-dark').style = 'color: black !important;';
-//   document.querySelector('.btn1').style = 'color: black !important;';
-//   document.querySelector('.btn2').style = 'color: black !important;';
-//   document.querySelector('.friends-dark').style =
-//   'background-color: #252525 !important; color: white;';
-// }
-
-// function lightTheme() {
-//   document.querySelector('.dark-mode').style = '';
-//   document.querySelector('.dark-menu').style = '';
-//   document.querySelector('.dropdown-item').style = '';
-//   document.querySelector('.dark-nav').style = '';
-//   document.querySelector('.dark-m-header').style = '';
-//   document.querySelector('.dark-m-body').style = '';
-//   document.querySelector('.dark-footer').style = '';
-//   document.querySelector('.btn-dark').style = 'color: black !important;';
-//   document.querySelector('.friends-dark').style = 'background-color: #fff !important';
-// }
-
 const darkTheme = () => {
   document.getElementById('dark').classList.add('dark-mode');
   document.getElementById('messages').classList.add('dark-mode');
