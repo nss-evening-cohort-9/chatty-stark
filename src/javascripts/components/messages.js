0cb3f272a96dab0f07e88278bc906c61179f50b4
Silvestre Luna <slvtrluna@gmail.com>
2019-04-29 19:09:42 -0500

    wip

--- src/javascripts/components/messages.js
+++ src/javascripts/components/messages.js
@@ -1,18 +1,26 @@
+import moment from 'moment';
+
 import data from '../helpers/data';
 import util from '../helpers/util';
 import bot from './chatbot';
 
 let messages = [];
 
+const addTimeStamp = () => {
+  const time = moment().format('LT');
+  console.error(time);
+  return time;
+};
+addTimeStamp();
 
 const domStringBuilder = (array) => {
   let domString = '';
   array.forEach((item) => {
-    domString += `<div class="message" id=${item.id}>`;
+    domString += `<div class="message-box" id=${item.id}>`;
     domString += '  <div class="message-heading d-flex align-items-center">';
-    domString += `    <img height="25" width="25" src=${item.imageUrl}>`;
-    domString += `    <div>${item.userName}</div>`;
-    domString += '    <div>1:00</div>';
+    domString += `    <img height="25" width="25" src=${item.imageUrl} class="pic">`;
+    domString += `    <div class="msg-name">${item.userName}</div>`;
+    domString += `    <div class="time" id="time">${item.timeStamp}</div>`;
     domString += `    <i id="delete" class="${item.id} fas fa-trash-alt"></i>`;
     domString += '  </div>';
     domString += '  <div class="message-body">';
