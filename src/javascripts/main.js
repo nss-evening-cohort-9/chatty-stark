8cb022512dc048196a0c3e308a20363b27903e18
Jeressia Williamson <jeressiajaytv@gmail.com>
2019-04-30 08:57:57 -0500

    responsive styling wip

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -3,6 +3,7 @@ import friendslist from './components/friends/friends';
 import themes from './components/darkmode/darkmode';
 import events from './helpers/attach-events';
 import bot from './components/chatbot';
+import readability from './components/textsize/readability';
 
 import 'bootstrap';
 import '../styles/main.scss';
@@ -11,6 +12,7 @@ const init = () => {
   onLoad.getData();
   friendslist.getFriends();
   themes.themeButtons();
+  readability.attachEvents();
   events.attachEvents();
   bot.getData();
 };
