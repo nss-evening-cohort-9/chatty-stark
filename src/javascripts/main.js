92add5465bed23912f9c957db9b55af5f6e5e15f
Samuel Trejo <samuel.trejo@s.tcatnashville.edu>
2019-04-30 21:31:40 -0500

    wip

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -5,12 +5,14 @@ import events from './helpers/attach-events';
 import bot from './components/chatbot';
 import readability from './components/textsize/readability';
 import dbInit from './helpers/database/db-init';
+import db from './helpers/database/retrieve-data';
 
 import 'bootstrap';
 import '../styles/main.scss';
 
 const init = () => {
   dbInit.firebaseSetup();
+  db.getData();
   onLoad.getData();
   friendslist.getFriends();
   themes.themeButtons();
